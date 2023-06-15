83ce87e5a1f53b3524538cbfb022f7a33b02d399
Shuto Tamaoka <shuto@shutotamaoka.xyz>
2023-06-15 20:37:51 +0900

    update: sorry hara-kun

--- ros/setup-ros-noetic.sh
+++ ros/setup-ros-noetic.sh
@@ -1,14 +1,54 @@
 #!/bin/sh
 
+# copyright Ueda Ryuichi
+# https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu20.04_desktop
+
 set -eu
 
-sudo apt update -y
+UBUNTU_VER=$(lsb_release -sc)
+ROS_VER=noetic
+[ "$UBUNTU_VER" = "focal" ] || exit 1
+
+echo "deb http://packages.ros.org/ros/ubuntu $UBUNTU_VER main" > /tmp/$$-deb
+sudo mv /tmp/$$-deb /etc/apt/sources.list.d/ros-latest.list
+
 sudo apt install -y curl
+curl -k https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo apt-key add -
+sudo apt update || echo ""
 
-# https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu20.04_desktop
-curl -sLf https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu20.04_desktop/raw/master/step1.bash | bash
+sudo apt install -y ros-${ROS_VER}-desktop-full
 
-[ -d ~/catkin_ws/src ] || mkdir -p ~/catkin_ws/src
+ls /etc/ros/rosdep/sources.list.d/20-default.list && sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
+sudo apt install python3-pip
+sudo -H pip3 install rosdep
+sudo rosdep init 
+sudo rosdep update
+
+sudo apt install -y python3-rosinstall
+sudo apt install -y build-essential
+sudo apt install -y rviz
+
+grep -F "source /opt/ros/$ROS_VER/setup.bash" ~/.bashrc ||
+echo "source /opt/ros/$ROS_VER/setup.bash" >> ~/.bashrc
+
+grep -F "ROS_MASTER_URI" ~/.bashrc ||
+echo "export ROS_MASTER_URI=http://localhost:11311" >> ~/.bashrc
+
+grep -F "ROS_HOSTNAME" ~/.bashrc ||
+echo "export ROS_HOSTNAME=localhost" >> ~/.bashrc
+
+sudo mkdir -p $USER:$USER $HOME/.ros/
+sudo chown $USER:$USER $HOME/.ros/ -R 
+
+### instruction for user ###
+#set +xv
+
+echo '***INSTRUCTION*****************'
+echo '* do the following command    *'
+echo '* $ source ~/.bashrc          *'
+echo '* after that, try             *'
+echo '* $ LANG=C roscore            *'
+echo '*******************************'
 
 grep -F "[ -f ~/catkin_ws/devel/setup.bash ] && source ~/catkin_ws/devel/setup.bash" ~/.bashrc ||
     echo "[ -f ~/catkin_ws/devel/setup.bash ] && source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
